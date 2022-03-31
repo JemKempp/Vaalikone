9e3031758d9b1998101f76894df1a7eea4045148
JemKempp <jemina.kemppainen@student.hamk.fi>
2022-03-31 16:50:04 +0300

    add folders

---
+++ src/main/java/app/model/RemoveQuestions.java
@@ -0,0 +1,37 @@
+package app.model;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+@WebServlet(
+		name = "RemoveQuestions",
+		urlPatterns = {"/RemoveQuestions"}
+		)
+public class RemoveQuestions extends HttpServlet {
+	private Dao dao;
+	public void init() {
+		dao=new Dao("jdbc:mysql://localhost:3306/vaalikone", "admin", "salasana");
+	}
+	
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+	throws IOException, ServletException {
+		
+		String id=request.getParameter("id");
+		ArrayList<Question> list=null;
+		if (dao.getConnection()) {
+			
+		list=dao.RemoveQuestion(id);
+		System.out.println("connected");
+		System.out.println(list);
+		
+	}
+		else {
+			System.out.println("No connection to database");
+		}
+		request.setAttribute("questionlist", list);
+		RequestDispatcher rd=request.getRequestDispatcher("/jsp/RemoveQuestions.jsp");
+		rd.forward(request,  response);
+		
+	}
+}
